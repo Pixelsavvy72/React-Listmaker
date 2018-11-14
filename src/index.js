d42ed5c086b973deef4a64e1431611e26baea4c5
Craig Pirrall <craigpir@gmail.com>
2018-11-14 15:49:44 +0900

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,85 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './app.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function FormFields(props) {
+  return(
+    <div>
+      <input id="myInput" onChange={props.changed} placeholder="Please enter an item"/>
+      <button onClick={props.added}>ADD</button>
+    </div>
+  )
+}
+
+function ListItem(props) {
+  return(
+    <li onClick={props.remove}>{props.value}</li>
+  )
+}
+
+class ShoppingList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentText: "",
+      listItems: []
+    }
+  }
+
+  onFieldChangedHandler = (event) => {
+    this.setState({currentText: event.target.value})
+  }
+
+  addItemHandler = () => {
+    let newList = [...this.state.listItems];
+    newList.push(this.state.currentText);
+    this.setState({
+      currentText: "",
+      listItems: newList
+    });
+    let myInput = document.getElementById("myInput");
+    myInput.value = "";
+  }
+
+  removeItemHandler = (index) => {
+    const items = [...this.state.listItems];
+    items.splice(index, 1);
+    this.setState({listItems: items});
+  }
+  
+  render() {
+    let items = this.state.listItems.map((item, index) => {
+      return(
+        <ListItem 
+          remove={() => this.removeItemHandler(index)}
+          value={item} 
+          key={Math.random() * Math.random()}
+        />
+      )
+    });
+
+    return(
+      <div>
+        <h1>List Items:</h1>
+        <p>(click item to remove)</p>
+        <ul>
+          {items}
+        </ul>
+        <FormFields 
+          added={this.addItemHandler} 
+          changed={this.onFieldChangedHandler}
+        />
+      </div>
+    );
+  }
+}
+
+
+
+
+
+ReactDOM.render(
+  <ShoppingList />,
+  document.getElementById('root')
+);
